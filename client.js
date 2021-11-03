9bae825bf500c7abb411a7590e6cd6663753f882
vagrant <aalibarre@gmail.com>
2021-11-03 23:12:22 +0000

    snake

--- client.js
+++ client.js
@@ -12,7 +12,7 @@ const connect = function () {
     // interpret incoming data as text
     conn.setEncoding("utf8");
     conn.write("Name: AAB");
-
+    conn.write("Move: up");
 
 
 
@@ -31,3 +31,7 @@ const connect = function () {
 
 module.exports = connect;
   
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
