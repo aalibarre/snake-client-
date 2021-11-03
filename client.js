6b787bc164b530cbfa1775f5819789a305c03919
ASA Dev <asadvlpr@gmail.com>
2021-11-03 18:59:58 -0400

    lol

--- client.js
+++ client.js
@@ -3,21 +3,31 @@ const net = require("net");
 const connect = function () {
     const conn = net.createConnection({
       host: '165.227.47.243',
-      port: 50541,
+      port: 50541
     });
-  
+
+    conn.on("connect", () => {
+      console.log("sucessfully connected to game server");
+   });
     // interpret incoming data as text
     conn.setEncoding("utf8");
-    conn.on("connect", () => {
-    console.log("incoming data - Client is connected to the server");
-    });
+    conn.write("Name: AAB");
+
+
+
+
+
+
+
+
+
+
     return conn;
   };
 
+ 
+  
 
 
-  console.log("Connecting ...");
-
-
-module.exports = {connect};
+module.exports = connect;
   
