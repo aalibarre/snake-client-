9ab8e7c30f63a1ee709fae430dd22c090a288b39
ASA Dev <asadvlpr@gmail.com>
2021-11-03 16:40:54 -0400

    client and play files ES6

--- play.js
+++ play.js
@@ -1,19 +1,8 @@
 const net = require("net");
-
+const { connect } = require('./client');
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-  console.log("incoming data - Client is connected to the server");
-  });
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
+
