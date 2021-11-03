59d49746b2007e6d29632cecb410516f3d7c7106
vagrant <aalibarre@gmail.com>
2021-11-03 23:25:25 +0000

    control c

--- play.js
+++ play.js
@@ -2,6 +2,22 @@ const net = require("net");
 const  connect  = require('./client');
 // establishes a connection with the game server
 
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    stdin.on("data", handleUserInput);
+    return stdin;
+  };
+
+
+  const handleUserInput = function (data) {
+    if (data === '\u0003') {
+        process.exit();
+      }
+  };
+setupInput();
 
 console.log("Connecting ...");
 connect();
